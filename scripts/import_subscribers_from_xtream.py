fd12df4f9925f07cb97fa7273db86f1216aa01ce
topilski <atopilski@gmail.com>
2019-09-20 01:24:33 -0400

    Without max connections

--- scripts/import_subscribers_from_xtream.py
+++ scripts/import_subscribers_from_xtream.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
 
     mycursor = mydb.cursor(dictionary=True)
 
-    sql = 'SELECT username,password,created_at,exp_date,max_connections FROM users'
+    sql = 'SELECT username,password,created_at,exp_date FROM users'
 
     mycursor.execute(sql)
 
@@ -61,7 +61,7 @@ if __name__ == '__main__':
         exp_date = sql_entry['exp_date']
         if exp_date:
             new_user.exp_date = datetime.fromtimestamp(exp_date)
-        dev = Device(name='Xtream', max_connections=sql_entry['max_connections'])
+        dev = Device(name='Xtream')
         new_user.add_device(dev)
         # save
         new_user.add_server(server)
