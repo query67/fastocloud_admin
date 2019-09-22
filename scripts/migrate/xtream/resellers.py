47a48409ab2b437d9869627512ffd51c39c9a1dc
topilski <atopilski@gmail.com>
2019-09-22 03:36:48 -0400

    Email for resellers

--- scripts/migrate/xtream/resellers.py
+++ scripts/migrate/xtream/resellers.py
@@ -4,12 +4,12 @@ from app.service.service import ServiceSettings, ProviderPair
 
 def import_resellers_to_server(db, server: ServiceSettings):
     cursor = db.cursor(dictionary=True)
-    sql = 'SELECT username,password from reg_users'
+    sql = 'SELECT username,email,password from reg_users'
     cursor.execute(sql)
     sql_providers = cursor.fetchall()
 
     for sql_entry in sql_providers:
-        new_user = ProviderUser.make_provider(email=sql_entry['username'], password=sql_entry['username'], country='US')
+        new_user = ProviderUser.make_provider(email=sql_entry['email'], password=sql_entry['email'], country='US')
         new_user.status = ProviderUser.Status.ACTIVE
         new_user.save()
 
