fee810961aa3a7e30311a68b190114b837b2ba56
topilski <atopilski@gmail.com>
2019-09-22 03:24:14 -0400

    Fixes for resselers import

--- scripts/migrate/xtream/resellers.py
+++ scripts/migrate/xtream/resellers.py
@@ -1,5 +1,5 @@
 from app.home.entry import ProviderUser
-from app.service.service import ServiceSettings
+from app.service.service import ServiceSettings, ProviderPair
 
 
 def import_resellers_to_server(db, server: ServiceSettings):
@@ -11,7 +11,10 @@ def import_resellers_to_server(db, server: ServiceSettings):
     for sql_entry in sql_providers:
         new_user = ProviderUser.make_provider(email=sql_entry['username'], password=sql_entry['username'], country='US')
         new_user.status = ProviderUser.Status.ACTIVE
+        new_user.save()
+
+        admin = ProviderPair(new_user.id, ProviderPair.Roles.ADMIN)
+        server.add_provider(admin)
         new_user.add_server(server)
-        server.add_provider(new_user)
 
     cursor.close()
